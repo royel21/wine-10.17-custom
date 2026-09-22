eda679ab6c2503c86a9dd4722ef0087575a041c0
royel21 <royelconsoro@hotmail.com>
2026-09-22 20:31:29 -0300

    0059-kernel32-tests-Disable-heap-tests-which-are-now-cras

--- dlls/winex11.drv/init.c
+++ dlls/winex11.drv/init.c
@@ -317,17 +317,19 @@ static void x11drv_client_surface_detach( struct client_surface *client )
 
 static void client_surface_update_geometry( HWND hwnd, struct x11drv_client_surface *surface )
 {
+    UINT dpi = NtUserGetDpiForWindow( hwnd ); /* use window DPI here, DPI scaling is handled through offscreen presentation */
     HWND origin = hwnd, toplevel = NtUserGetAncestor( hwnd, GA_ROOT );
     XWindowChanges changes = surface->changes;
     struct x11drv_win_data *data;
     int mask = 0;
     RECT rect;
 
-    if (NtUserGetPresentRect( toplevel, &rect, -1 /* raw dpi */ )) origin = hwnd;
-    else if (!NtUserGetClientRect( hwnd, &rect, NtUserGetWinMonitorDpi( hwnd, MDT_RAW_DPI ) )) return;
-    NtUserMapWindowPoints( origin, toplevel, (POINT *)&rect, 2, NtUserGetWinMonitorDpi( hwnd, MDT_RAW_DPI ) );
-     if ((data = get_win_data( toplevel )))
-     {
+    if (NtUserGetPresentRect( hwnd, &rect, dpi )) OffsetRect( &rect, -rect.left, -rect.top );
+    else if (!NtUserGetClientRect( hwnd, &rect, dpi )) return;
+    else NtUserMapWindowPoints( origin, toplevel, (POINT *)&rect, 2, dpi );
+
+    if ((data = get_win_data( toplevel )))
+    {
         OffsetRect( &rect, data->rects.client.left - data->rects.visible.left,
                     data->rects.client.top - data->rects.visible.top );
         release_win_data( data );
@@ -442,7 +444,7 @@ static void X11DRV_client_surface_present( struct client_surface *client, HDC hd
     {
         region = get_dc_monitor_region( hwnd, hdc ); /* otherwise use the window region for clipping rules */
         if (!NtUserGetClientRect( hwnd, &rect_dst, NtUserGetWinMonitorDpi( hwnd, MDT_RAW_DPI ) )) goto done;
-        NtUserMapWindowPoints( hwnd, toplevel, (POINT *)&rect_dst, 2, NtUserGetWinMonitorDpi( hwnd, MDT_RAW_DPI ) );
+        OffsetRect( &rect_dst, -rect_dst.left, -rect_dst.top );
     }
 
      if ((data = get_win_data( toplevel )))
