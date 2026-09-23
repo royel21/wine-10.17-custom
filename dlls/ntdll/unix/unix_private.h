aafffd3dfa9d9f0dfd49441463ee60fce05f59c0
royel21 <royelconsoro@hotmail.com>
2026-09-23 16:20:43 -0400

    0179-kernel32-Don-t-use-export-forwarding-for-ntdll-funct

--- dlls/ntdll/unix/unix_private.h
+++ dlls/ntdll/unix/unix_private.h
@@ -202,6 +202,7 @@ extern HANDLE keyed_event;
 extern timeout_t server_start_time;
 extern sigset_t server_block_set;
 extern struct _KUSER_SHARED_DATA *user_shared_data;
+extern ULONG process_cookie;
 
 extern void init_environment(void);
 extern void init_startup_info(void);
